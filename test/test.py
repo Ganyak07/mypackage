07e7fa97ab1d8da1cd51418768a948baeb02e943
Ganiyat Yakub <yakubganiyat@gmail.com>
2024-02-24 23:15:59 -0800

    second commit

---
+++ test/test.py
@@ -0,0 +1,10 @@
+from mypackage import myModule
+
+def test_top_n():
+    """
+    make sure top_n works correctly
+    """
+    
+    assert myModule.top_n([8, 3, 2, 7, 4], 3) == [8, 7, 4], 'incorrect'
+    assert myModule.top_n([10, 1, 12, 9, 2], 2) == [12, 10], 'incorrect'
+    assert myModule.top_n([1, 2, 3, 4, 5], 5) == [5, 4, 3, 2, 1], 'incorrect'
